ae83ac34e6498f9895ef1d6b4c5cde81b1ec9d50
Gokul Kartha <kartha.gokul@gmail.com>
2025-07-11 21:54:50 +0200

    fixes

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+sudo apt install xorriso genisoimage live-build
 sudo lb clean --all
+sudo lb config \
+  --distribution noble \
+  --binary-images iso-hybrid \
+  --debian-installer live \
+  --archive-areas "main restricted universe multiverse" \
+  --mirror-bootstrap http://archive.ubuntu.com/ubuntu/ \
+  --mirror-binary http://archive.ubuntu.com/ubuntu/ \
+  --mode ubuntu
 sudo lb build
